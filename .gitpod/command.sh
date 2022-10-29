74cad4343eaca87f79fa8bc12e300dfc007976ea
Marcin Maruszewski <m.marusz@gmail.com>
2022-10-29 00:11:10 +0000

    Do the magic with permissions for the last time.

--- .gitpod/command.sh
+++ .gitpod/command.sh
@@ -13,3 +13,7 @@ make drush deploy
 
 # Get a one-time login URL
 make drush uli
+
+# This is the last time, I prommise ✨
+sudo setfacl -dR -m u:$(whoami):rwX -m u:1000:rwX .
+sudo setfacl -R -m u:$(whoami):rwX -m u:1000:rwX  .
